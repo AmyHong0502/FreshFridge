651db44dec23d51f609baca94e69e06f76440b2e
Jonah Marshall <JonahMarshall@protonmail.com>
2018-04-26 17:11:04 -0700

    Search bar added

---
+++ searchResults.js
@@ -0,0 +1,17 @@
+function filterFunction() {
+  
+	var input, filter, ul, li, a, i;
+	input = document.getElementById("fridgeFilter");
+	filter = input.value.toUpperCase();
+	div = document.getElementById("myDropdown");
+	a = div.getElementsByTagName("a");
+  
+	for (i = 0; i < a.length; i++) {
+		if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+			a[i].style.display = "block";
+		} else {
+			a[i].style.display = "none";
+		}
+		
+	}
+}
