f91b52b92f143e580c634aeabbab562b87fe11be
Amy Hong <amyhong0502@gmail.com>
2018-05-06 19:31:13 -0700

    feat: add Mongoose & start pug

--- express/freshfridge/routes/index.js
+++ express/freshfridge/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'FreshFridge' });
+  res.render('index', { title: 'FreshFridge', subtitle: 'Save food and save money.' });
 });
 
 module.exports = router;
