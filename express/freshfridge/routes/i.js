d2ed98c4bda36a1a16c78e441b7aa4003eb4faf2
Amy Hong <amyhong0502@gmail.com>
2018-05-09 17:20:59 -0700

    feat: add function to talk to DB

--- express/freshfridge/routes/i.js
+++ express/freshfridge/routes/i.js
@@ -1,17 +1,59 @@
 var express = require('express');
+var Fridge = require('../models/fridge');
+var ShoppingList = require('../models/shoppinglist');
+
 var router = express.Router();
 
+var async = require('async');
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('My page. Respond with a resource.');
+router.get('/', function (req, res, next) {
+    async.parallel({
+        fridge_count: function (callback) {
+            Fridge.count({}, callback);
+        },
+        shoppingList_count: function (callback) {
+            ShoppingList.count({}, callback);
+        }
+    }, function (err, results) {
+        res.render('i', {
+            title: 'FreshFridge',
+            subtitle: 'Save food and save money.',
+            error: err,
+            data: results
+        });
+    });
 });
 
-router.get('/fridge', function(req, res, next) {
-  res.send('fridge page. Respond with a resource.');
+router.get('/fridge', function (req, res, next) {
+    async.parallel({
+        fridge_count: function (callback) {
+            Fridge.count({}, callback);
+        },
+    }, function (err, results) {
+        res.render('fridge', {
+            title: 'FreshFridge',
+            subtitle: 'Save food and save money.',
+            error: err,
+            data: results
+        });
+    });
 });
 
-router.get('/shopping-list', function(req, res, next) {
-  res.send('shopping-list page. Respond with a resource.');
+router.get('/shopping-list', function (req, res, next) {
+    async.parallel({
+
+        shoppingList_count: function (callback) {
+            ShoppingList.count({}, callback);
+        }
+    }, function (err, results) {
+        res.render('shopping-list', {
+            title: 'FreshFridge',
+            subtitle: 'Save food and save money.',
+            error: err,
+            data: results
+        });
+    });
 });
 
 module.exports = router;
