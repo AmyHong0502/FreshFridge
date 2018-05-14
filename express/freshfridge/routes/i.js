67dcf937e2ef4554269d5fbaca1219a5fb0e6faf
Amy Hong <amyhong0502@gmail.com>
2018-05-14 14:03:11 -0700

    feat: add skeleton shopping-list page

--- express/freshfridge/routes/i.js
+++ express/freshfridge/routes/i.js
@@ -1,17 +1,56 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('i', { title: 'FreshFridge', subtitle: 'Save food and save money.' });
-});
+// Require controller modules
+let fridge_controller = require('../controllers/fridgeController');
+let shoppinglist_controller = require('../controllers/shoppinglistController');
 
-router.get('/fridge', function(req, res, next) {
-  res.render('fridge', { title: 'FreshFridge', subtitle: 'Save food and save money.' });
-});
+let async = require('async');
 
-router.get('/shopping-list', function(req, res, next) {
-  res.render('shopping-list', { title: 'FreshFridge', subtitle: 'Save food and save money.' });
+// GET /i page
+// router.get('/', function (req, res) {
+//     async.parallel({
+//         fridge_count: function (callback) {
+//             Fridge.count({}, callback);
+//         },
+//         shoppingList_count: function (callback) {
+//             ShoppingList.count({}, callback);
+//         }
+//     }, function (err, results) {
+//         res.render('i', {
+//             title: 'FreshFridge',
+//             subtitle: 'Save food and save money.',
+//             error: err,
+//             data: results
+//         });
+//     });
+// });
+
+// Fridge Routes //
+router.get('/fridge', fridge_controller.fridge_list);
+
+router.post('/fridge/process', fridge_controller.fridge_process);
+
+// router.get('/fridge', function (req, res, next) {
+//     async.parallel({
+//         fridge_count: function (callback) {
+//             Fridge.count({}, callback);
+//         },
+//     }, function (err, results) {
+//         res.render('fridge', {
+//             title: 'FreshFridge',
+//             subtitle: 'Save food and save money.',
+//             error: err,
+//             data: results
+//         });
+//     });
+// });
+
+router.get('/shopping-list', function (req, res, next) {
+    res.render('shopping-list', {
+        title: 'FreshFridge',
+        subtitle: 'Save food and save money.'
+    });
 });
 
 module.exports = router;
