dc8b4d06483dca1febd943e2bf5e9b7bdf6da04b
Jonah Marshall <JonahMarshall@protonmail.com>
2018-05-16 12:46:53 -0700

    Bug fix on two word preset resolve, file to be tested by caleb

--- Site/fridgeButton.js
+++ Site/fridgeButton.js
@@ -1,16 +1,25 @@
 $(document).ready(function () {
+	
+	//Preset item button
 	$(".itemButton").click(function() {
 		$(this).parent().toggleClass("faded");
 		
 		if(!($(this).parent().hasClass("faded"))){
-			$("#ingredientsUL").append('<li id=' + $(this).next("figcaption").html() + ' class="ingredientLi">' + $(this).next("figcaption").html() +' <a class="remove">&times;</a></li>'); 
+			$("#ingredientsUL").append('<li id=' + $(this).next("figcaption").text() + ' class="ingredientLi">' + $(this).next("figcaption").text() +' <a class="remove">&times;</a></li>'); 
 		}
 		else{
-			$('#ingredientsUL #'+$(this).next("figcaption").html()).remove();
+			$('#ingredientsUL li:contains('+$(this).next("figcaption").text()+')').remove();
 		}
 	});
+	
+	//Close button
 	$(document).on("click", "a.remove" , function() {
-		$("figcaption:contains("+$(this).parent().text()+")").parent().toggleClass("faded");
+		/*window.alert($(this).parent().contents().filter(function(){
+			return this.nodeType !== 1;
+		}).text());*/
+		$("figcaption:contains("+$(this).parent().contents().filter(function(){
+			return this.nodeType !== 1;
+		}).text()+")").parent().toggleClass("faded");
         $(this).parent().remove();
     });
 });
