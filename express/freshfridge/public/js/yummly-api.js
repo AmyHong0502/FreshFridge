2d9ac1eaa53a61398671e7777f2109a7700604fe
Amy Hong <amyhong0502@gmail.com>
2018-05-18 08:53:39 -0700

    feat: restore Easter egg & beautify recipe search at index

--- express/freshfridge/public/js/yummly-api.js
+++ express/freshfridge/public/js/yummly-api.js
@@ -28,7 +28,7 @@ function keepIngredients() {
 function generateIngredientInputs(ingredients) {
     let location = document.getElementById('ingredients');
 
-    while(location.firstChild) {
+    while (location.firstChild) {
         location.removeChild(location.firstChild);
     }
 
@@ -168,38 +168,54 @@ function handleError(status) {
 // Adds new list item to array
 // Used at index.pug, not here.
 function listNewIngredient() {
-    console.log("listNewIngredient called");
-
     let inputValue = document.getElementById("myInput").value;
     if (inputValue === null || inputValue === '') {
         alert("You must write something!");
         return;
-    }
-
-    let li = document.createElement("li");
-    let t = document.createTextNode(inputValue);
-    li.setAttribute("class", "ingredientLi");
-    li.appendChild(t);
-
-    let span = document.createElement("SPAN");
-    let txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-            let div = this.parentElement;
-            div.remove();
+    } else if (inputValue === 'pairs' || inputValue === 'pair') {
+        let x = document.createElement("IMG");
+        x.setAttribute("src", "/img/pairs.jpg");
+        x.setAttribute("width", "200px");
+        x.setAttribute("height", "228px");
+        document.getElementById("functional").appendChild(x);
+        x.onclick = function () {
+            let div = x;
+            div.style.display = "none";
         }
+    } else {
+        let li = document.createElement("li");
+        let t = document.createTextNode(inputValue);
+        li.id = "ingredientLi";
+        li.appendChild(t);
+
+        let span = document.createElement("SPAN");
+        let txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        li.appendChild(span);
+
+        let ul = document.getElementById("ingredientsUL");
+        ul.appendChild(li);
+
+        let closeSpans = document.getElementsByClassName('close');
+        for (let i = 0; i < closeSpans.length; i++) {
+            closeSpans[i].onclick = function () {
+                console.log("1st method");
+                let div = this.parentElement;
+                div.remove();
+            }
+        }
+
+        for (let i = 0; i < close.length; i++) {
+            close[i].onclick = function() {
+                console.log("second method");
+                let div = this.parentElement;
+                div.remove();
+            }
+        }
+
+        generateIngredientInputs(extractUserIngredients());
     }
-
-
-    let ul = document.getElementById("ingredientsUL");
-    ul.appendChild(li);
-
-    generateIngredientInputs(extractUserIngredients());
-
     document.getElementById("myInput").value = "";
 }
 
